d043768450d46923daad7aec2d90583ffe045c99
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-24 02:29:16 -0300

    classe Trabalhador, ligações das classes

--- src/entities/ContratoHora.java
+++ src/entities/ContratoHora.java
@@ -41,5 +41,8 @@ public class ContratoHora {
 	public void setHoras(Integer horas) {
 		this.horas = horas;
 	}
+	public double valorTotal() {
+		return valorPorHora * horas;
+	}
 
 }
---
+++ src/entities/Trabalhador.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.NivelTrabalhador;
+
+public class Trabalhador {
+	private String nome;
+	private NivelTrabalhador nivel;
+	private Double salarioBase;
+	
+	//Associações: Trabalhador
+	private Departamento departamento;
+	private List<ContratoHora> contratos = new ArrayList<>();
+	
+	public Trabalhador() {
+		
+	}
+	// gerar o construtor sem a lista contratos
+
+	public Trabalhador(String nome, NivelTrabalhador nivel, Double salarioBase, Departamento departamento) {
+		
+		this.nome = nome;
+		this.nivel = nivel;
+		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public NivelTrabalhador getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(NivelTrabalhador nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<ContratoHora> getContratos() {
+		return contratos;
+	}
+
+	
+	public void addContrato(ContratoHora contrato) {
+		contratos.add(contrato);
+	}
+	public void removeContrato(ContratoHora contrato) {
+		contratos.remove(contrato);
+	}
+	/*percorrer os contratos deste funcionário, testando quais os contratos que são deste mês e ano
+	e se o contrato for deste mes e ano vai entrar na soma*/
+	public double calcular (int ano, int mes) {
+		double soma = salarioBase;	
+		Calendar cal = Calendar.getInstance();
+		for (ContratoHora c : contratos) {
+			cal.setTime(c.getDate());
+			int c_ano = cal.get(Calendar.YEAR);
+		    int c_mes = 1 + cal.get(Calendar.MONTH);
+			if (ano == c_ano && mes == c_mes) {
+				soma +=  c.valorTotal();
+			}
+		}
+		return soma;
+	}
+	
+
+}
